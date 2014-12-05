342bad0e062312da3a90afeaed36f588acbc9b04
cidro <pablo.canaleshernandez@gmail.com>
2014-12-05 14:35:32 -0500

    ups, moved wrong content

--- jbx/recipes/core.rb
+++ jbx/recipes/core.rb
@@ -24,6 +24,36 @@ execute 'Deploy Script' do
     command 'sh deploy.sh'
 end
 
+#Update application credentials
+credentials_file = "#{node['jbx']['core']['path']}/config/credentials.json"
+credentials_file_template = "credentials.json.erb"
+
+template credentials_file do
+  source credentials_file_template
+  variables ({
+      "mysql_read_host"         => 'read1.mysql.jumbleberry.com',
+      "mysql_read_username"     => node['jbx']['credentials']['mysql_read']['username'],
+      "mysql_read_password"     => node['jbx']['credentials']['mysql_read']['password'],
+      "mysql_read_database"     => node['jbx']['credentials']['mysql_read']['dbname'],
+
+      "mysql_write_host"        => 'write1.mysql.jumbleberry.com',
+      "mysql_write_username"    => node['jbx']['credentials']['mysql_write']['username'],
+      "mysql_write_password"    => node['jbx']['credentials']['mysql_write']['password'],
+      "mysql_write_database"    => node['jbx']['credentials']['mysql_write']['dbname'],
+
+      "hitpath_host"            => node['jbx']['credentials']['hitpath']['host'],
+      "hitpath_username"        => node['jbx']['credentials']['hitpath']['username'],
+      "hitpath_password"        => node['jbx']['credentials']['hitpath']['password'],
+      "hitpath_database"        => node['jbx']['credentials']['hitpath']['dbname'],
+
+      "redis_host"              => 'read1.redis.jumbleberry.com',
+      "redis_port"              => node['jbx']['credentials']['redis']['port'],
+
+      "crypt"                   => node['jbx']['credentials']['crypt'],
+      "raygun"                  => node['jbx']['credentials']['raygun']
+    })
+end
+
 # Run database migrations
 execute 'Database migrations' do
   cwd "#{node['jbx']['core']['path']}/application/cli"
--- jbx/recipes/hosts.rb
+++ jbx/recipes/hosts.rb
@@ -1,29 +1,20 @@
-#Update application credentials
-credentials_file = "#{node['jbx']['core']['path']}/config/credentials.json"
-credentials_file_template = "credentials.json.erb"
-
-template credentials_file do
-  source credentials_file_template
+# Add custom domains
+template '/etc/hosts' do
+  source 'hosts.erb'
   variables ({
-      "mysql_read_host"         => 'read1.mysql.jumbleberry.com',
-      "mysql_read_username"     => node['jbx']['credentials']['mysql_read']['username'],
-      "mysql_read_password"     => node['jbx']['credentials']['mysql_read']['password'],
-      "mysql_read_database"     => node['jbx']['credentials']['mysql_read']['dbname'],
-
-      "mysql_write_host"        => 'write1.mysql.jumbleberry.com',
-      "mysql_write_username"    => node['jbx']['credentials']['mysql_write']['username'],
-      "mysql_write_password"    => node['jbx']['credentials']['mysql_write']['password'],
-      "mysql_write_database"    => node['jbx']['credentials']['mysql_write']['dbname'],
-
-      "hitpath_host"            => node['jbx']['credentials']['hitpath']['host'],
-      "hitpath_username"        => node['jbx']['credentials']['hitpath']['username'],
-      "hitpath_password"        => node['jbx']['credentials']['hitpath']['password'],
-      "hitpath_database"        => node['jbx']['credentials']['hitpath']['dbname'],
-
-      "redis_host"              => 'read1.redis.jumbleberry.com',
-      "redis_port"              => node['jbx']['credentials']['redis']['port'],
-
-      "crypt"                   => node['jbx']['credentials']['crypt'],
-      "raygun"                  => node['jbx']['credentials']['raygun']
-    })
+        'hostname'              => "#{node['fqdn']} #{node['hostname']}",
+        'mysql_read_host_1'     => node['jbx']['credentials']['mysql_read']['host'],
+        'mysql_read_host_2'     => node['jbx']['credentials']['mysql_read']['host'],
+        'mysql_write_host_1'    => node['jbx']['credentials']['mysql_write']['host'],
+        'mysql_write_host_2'    => node['jbx']['credentials']['mysql_write']['host'],
+        'redis_host_read_1'     => node['jbx']['credentials']['redis']['host'],
+        'redis_host_read_2'     => node['jbx']['credentials']['redis']['host'],
+        'redis_host_write_1'    => node['jbx']['credentials']['redis']['host'],
+        'redis_host_write_2'    => node['jbx']['credentials']['redis']['host'],
+        'gearman_host_1'        => node['jbx']['credentials']['gearman']['host'],
+        'gearman_host_2'        => node['jbx']['credentials']['gearman']['host']
+  })
+  mode 0644
+  owner 'root'
+  group 'root'
 end
