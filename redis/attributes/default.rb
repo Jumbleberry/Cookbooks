0412854416ea3f921369a5cd9606c367f4bd5fe3
vagrant <vagrant@jbx-web-server.dev.jumbleberry.com>
2014-12-10 20:16:29 +0000

    removed hardcoded directory names and ports

--- redis/attributes/default.rb
+++ redis/attributes/default.rb
@@ -3,4 +3,6 @@ default['redis']['git_ref'] = '2.8'
 default['redis']['master_hostname'] = 'redis1.jumbleberry.com'
 default['redis']['path'] = '/opt/redis'
 default['redis']['config'] = '/etc/redis'
-
+default['redis']['log'] = '/var/log/redis'
+default['redis']['basePort'] = 6379
+default['redis']['sentinelPort'] = 26379
