a874e28dae4fa9ff9232ba1b4af07c39ad54fd86
Ian Elliott <ian@elliian.com>
2014-12-18 14:10:22 -0500

    S3FS Updated to work as non root

---
+++ s3fs-fuse/attributes/default.rb
@@ -0,0 +1,11 @@
+default[:s3fs_fuse][:s3_url] = 'https://s3.amazonaws.com'
+default[:s3fs_fuse][:s3_key] = ''
+default[:s3fs_fuse][:s3_secret] = ''
+default[:s3fs_fuse][:version] = '1.61'
+default[:s3fs_fuse][:no_upload] = false
+default[:s3fs_fuse][:mounts] = []
+default[:s3fs_fuse][:bluepill] = false
+default[:s3fs_fuse][:rc_mon] = false
+default[:s3fs_fuse][:maxmemory] = 100
+default[:s3fs_fuse][:fuse_mirror] = 'voxel'
+default[:s3fs_fuse][:packages] = []
