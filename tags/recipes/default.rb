f6e83c414c9470271ef6db79d097376cabb7cb07
Ian Elliott <ian@elliian.com>
2014-12-19 15:03:47 -0500

    Update key names

--- tags/recipes/default.rb
+++ tags/recipes/default.rb
@@ -4,8 +4,8 @@ if ( node.attribute?('ec2') && node.attribute?('aws') && node['aws'].attribute?(
     
     node['aws']['tags'].each do |key, array|
         aws_resource_tag node['ec2']['instance_id'] do
-          aws_access_key node['aws']['aws_access_key']
-          aws_secret_access_key node['aws']['aws_secret_key']
+          aws_access_key node['aws']['aws_access_key_id']
+          aws_secret_access_key node['aws']['aws_secret_access_key']
           tags(
               node['aws']['tags'][key]
           )
