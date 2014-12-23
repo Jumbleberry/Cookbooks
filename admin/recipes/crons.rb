9fc82448c0a0e066d0408fbda60880026c46487f
Ian Elliott <ian@elliian.com>
2014-12-23 16:25:26 -0500

    Add user to cron

--- admin/recipes/crons.rb
+++ admin/recipes/crons.rb
@@ -2,24 +2,28 @@
 cron "Sync Hitpath" do
   command "/usr/bin/php #{node[:admin][:path]}/cron_scripts/sync_hitpath.php"
   minute '*/3'
+  user 'www-data'
   action :create
 end
 
 cron "Sale Projections" do
   command "/usr/bin/php #{node[:admin][:path]}/cron_scripts/generate_projections.php"
   minute '2-59/3'
+  user 'www-data'
   action :create
 end
 
 cron "Notifications" do
   command "/usr/bin/php #{node[:admin][:path]}/cron_scripts/cap_notifications.php"
   minute '4-59/5'
+  user 'www-data'
   action :create
 end
 
 cron "CRM Data" do
   command "/usr/bin/php #{node[:admin][:path]}/cron_scripts/crm_stats.php"
   minute '0'
+  user 'www-data'
   action :create
 end
 
@@ -28,17 +32,20 @@ cron "CRM Data Report" do
   minute '30'
   hour '8'
   day '5,0'
+  user 'www-data'
   action :create
 end
 
 cron "Advertiser Prepay" do
   command "/usr/bin/php #{node[:admin][:path]}/cron_scripts/advertiser_limits.php"
   minute '0'
+  user 'www-data'
   action :create
 end
 
 cron "Pull Phone Data" do
   command "/usr/bin/php #{node[:admin][:path]}/cron_scripts/sync_phone.php"
   minute '45'
+  user 'www-data'
   action :create
 end
