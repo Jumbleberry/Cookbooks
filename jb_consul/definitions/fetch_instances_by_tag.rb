9ac1dddc882196cdf3817b199fc6a644e2b84c6b
Ian Elliott <ian@elliian.com>
2014-12-24 11:35:04 -0500

    Update syntax for bootstrap

--- jb_consul/definitions/fetch_instances_by_tag.rb
+++ jb_consul/definitions/fetch_instances_by_tag.rb
@@ -4,9 +4,7 @@ define :fetch_instances_by_tag, :tag_name => 'consul', :tag_value => 'bootstrap'
     stack_file_path = node['jb_consul']['stack_file_path']
 
 #    if node.has_key?("ec2")
-        check_consul_bootstrap = "aws ec2 describe-instances --filters 'Name=tag:#{params[:tag_name]},Values=#{params[:tag_value]}' > #{stack_file}"
-        execute check_consul_bootstrap do
-            cwd stack_file_path
-        end
+        check_consul_bootstrap = "aws ec2 describe-instances --filters 'Name=tag:#{params[:tag_name]},Values=#{params[:tag_value]}' > #{stack_file_path}#{stack_file}"
+        execute check_consul_bootstrap
 #    end
 end
