5846f83fd8be3071491772c7d8a6b3354d59ca34
Ian Elliott <ian@elliian.com>
2014-12-24 11:59:28 -0500

    Revert "remove definitions on jb_consul recipe"
    This reverts commit 0be59635da0580123f02cd3f347030cda8f5ed7c.

--- jb_consul/recipes/default.rb
+++ jb_consul/recipes/default.rb
@@ -1,14 +1,14 @@
 user = node['user']
-stack_file = node['jb_consul']['stack_file_path'] + node['jb_consul']['stack_file']
+stack_file = node['jb_consul']['stack_file']
+stack_file_path = node['jb_consul']['stack_file_path']
 min_servers = node['consul']['bootstrap_expect'].to_i
 
 #Check if there is any consul agent runing.
 #If we have an instance with the consul tag on cluster mode, it means that the bootstraping process is done
-if node.attribute?('ec2')
-    check_consul_bootstrap = "aws ec2 describe-instances --filters 'Name=tag:consul,Values=cluster' > #{stack_file}"
-    execute check_consul_bootstrap
+fetch_instances_by_tag 'consul' do
+    tag_value 'cluster'
 end
-instances_hash = JSON.parse(File.read("#{stack_file}"))
+instances_hash = JSON.parse(File.read("#{stack_file_path}#{stack_file}"))
 if instances_hash['Reservations'].count() > 0
     #Get the ip of the first instance with consul runing on it
     instance_ip = instances_hash['Reservations'][0]['Instances'][0]['PrivateIpAddress']
@@ -18,11 +18,10 @@ if instances_hash['Reservations'].count() > 0
 else
     #If we have no instance with the consul tag, then look for the bootstrap agents
     #Check if there is any bootstraping consul agent
-    if node.attribute?('ec2')
-        check_consul_bootstrap = "aws ec2 describe-instances --filters 'Name=tag:consul,Values=bootstrap' > #{stack_file}"
-        execute check_consul_bootstrap
+    fetch_instances_by_tag 'consul' do
+        tag_value 'bootstrap'
     end
-    instances_hash = JSON.parse(File.read("#{stack_file}"))
+    instances_hash = JSON.parse(File.read("#{stack_file_path}#{stack_file}"))
 
     if instances_hash['Reservations'].count() < min_servers
         #Mark this server to be started as bootstrap
@@ -35,42 +34,30 @@ else
         node.override['consul']['servers'] = bootstrap_servers
     end
 
-    if node.attribute?("ec2")
+#    if node.has_key?("ec2")
         #Set tag depending on how many bootstrap server we are runing
         if instances_hash['Reservations'].count() == (min_servers - 1)
             #Already reach the amount of bootstrap servers, bootstraping is done
             #so we change the tags from consul-bootstrap to consul
             instances_hash['Reservations'].each do | reservation |
                 reservation['Instances'].each do | instance |
-                    aws_resource_tag instance['InstanceId'] do
-                        aws_access_key node['aws']['aws_access_key_id']
-                        aws_secret_access_key node['aws']['aws_secret_access_key']
-                        tags (
-                            {'consul' => 'cluster'}
-                        )
-                        action :add
+                    update_tag instance['InstanceId'] do
+                        tag_name 'consul'
+                        tag_value 'cluster'
                     end
                 end
             end
             #Set the tag to the current runing instance
-            aws_resource_tag node['ec2']['instance_id'] do
-                aws_access_key node['aws']['aws_access_key_id']
-                aws_secret_access_key node['aws']['aws_secret_access_key']
-                tags (
-                    {'consul' => 'cluster'}
-                )
-                action :add
+            update_tag node['ec2']['instance_id'] do
+                tag_name 'consul'
+                tag_value 'cluster'
             end
         else
             #We haven't reached the needed bootstrap servers so we set the tag mark the server as another bootstrap
-            aws_resource_tag node['ec2']['instance_id'] do
-                aws_access_key node['aws']['aws_access_key_id']
-                aws_secret_access_key node['aws']['aws_secret_access_key']
-                tags (
-                    {'consul' => 'bootstrap'}
-                )
-                action :add
+            update_tag node['ec2']['instance_id'] do
+                tag_name 'consul'
+                tag_value 'bootstrap'
             end
         end
-    end
+#    end
 end
