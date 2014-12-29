7946ae5fefd78f29f9eca5166c5496f4019cf3b7
Ian Elliott <ian@elliian.com>
2014-12-29 10:27:45 -0500

    Full path

--- jb_consul/definitions/fetch_instances_by_tag.rb
+++ jb_consul/definitions/fetch_instances_by_tag.rb
@@ -5,7 +5,7 @@ define :fetch_instances_by_tag, :tag_name => 'consul', :tag_value => 'bootstrap'
     region = node['aws']['region']
 
 #    if node.has_key?("ec2")
-        check_consul_bootstrap = "aws --region #{region} --profile ~/.aws/config ec2 describe-instances --filters 'Name=tag:#{params[:tag_name]},Values=#{params[:tag_value]}' > #{stack_file_path}#{stack_file}"
+        check_consul_bootstrap = "aws --region #{region} --profile /home/ubuntu/.aws/config ec2 describe-instances --filters 'Name=tag:#{params[:tag_name]},Values=#{params[:tag_value]}' > #{stack_file_path}#{stack_file}"
         execute check_consul_bootstrap do
             user node['user']
         end
