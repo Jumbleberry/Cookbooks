bec2244708e764eba09911fc688766288da9e1a1
Ian Elliott <ian@elliian.com>
2014-12-29 10:05:04 -0500

    Specify AWS Region in CLI

--- jb_consul/definitions/fetch_instances_by_tag.rb
+++ jb_consul/definitions/fetch_instances_by_tag.rb
@@ -2,9 +2,10 @@ define :fetch_instances_by_tag, :tag_name => 'consul', :tag_value => 'bootstrap'
     params[:tag_name] ||= params[:name]
     stack_file = node['jb_consul']['stack_file']
     stack_file_path = node['jb_consul']['stack_file_path']
+    region = node['aws']['region']
 
 #    if node.has_key?("ec2")
-        check_consul_bootstrap = "aws ec2 describe-instances --filters 'Name=tag:#{params[:tag_name]},Values=#{params[:tag_value]}' > #{stack_file_path}#{stack_file}"
+        check_consul_bootstrap = "aws --region #{region} ec2 describe-instances --filters 'Name=tag:#{params[:tag_name]},Values=#{params[:tag_value]}' > #{stack_file_path}#{stack_file}"
         execute check_consul_bootstrap do
             user node['user']
         end
