19d0124cbc2b6bac7cd58a2f2d6119d07a172acf
Ian Elliott <ian@elliian.com>
2014-12-29 13:38:08 -0500

    Add debuging

--- jb_consul/definitions/fetch_instances_by_tag.rb
+++ jb_consul/definitions/fetch_instances_by_tag.rb
@@ -6,9 +6,13 @@ define :fetch_instances_by_tag, :tag_name => 'consul', :tag_value => 'bootstrap'
     ruby_block "get instances " + params[:tag_value] do
         block do
             ENV['HOME'] = node[:etc][:passwd][node['user']][:dir]
-            node.override['jb_consul']['instances'][params[:tag_value]] = JSON.parse(`aws --region #{region} --profile default ec2 describe-instances --filters 'Name=tag:#{params[:tag_name]},Values=#{params[:tag_value]}'` || {})
+            node.override['jb_consul']['instances'][params[:tag_value]] = JSON.parse(`aws --region #{region} --profile default ec2 describe-instances --filters 'Name=tag:#{params[:tag_name]},Values=#{params[:tag_value]}'` || '{"Reservations": []}')
         end
         action :run
+    end
+    log "debug" do
+        level :info
+        attribute node['jb_consul']
     end
 #    end
 end
