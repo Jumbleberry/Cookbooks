f516a03f43820b404443c5b0a5aad224c8bd02d7
cidro <pablo.canaleshernandez@gmail.com>
2014-12-29 13:28:01 -0500

    fix aws command

--- jb_consul/definitions/fetch_instances_by_tag.rb
+++ jb_consul/definitions/fetch_instances_by_tag.rb
@@ -5,7 +5,7 @@ define :fetch_instances_by_tag, :tag_name => 'consul', :tag_value => 'bootstrap'
 #    if node.has_key?("ec2")
     ruby_block "get instances " + params[:tag_value] do
         ENV['HOME'] = node[:etc][:passwd][node['user']][:dir]
-        node.override['jb_consul']['instances'][params[:tag_value]] = JSON.parse(`echo '{"Reservations":[]}'` || {})
+        node.override['jb_consul']['instances'][params[:tag_value]] = JSON.parse(`aws --region #{region} --profile default ec2 describe-instances --filters 'Name=tag:#{params[:tag_name]},Values=#{params[:tag_value]}'` || {})
     end
 #    end
 end
