cb55f36c1ae30cfc770cbbb25128ef11ec0be5e0
Ian Elliott <ian@elliian.com>
2014-12-29 15:55:28 -0500

    No default

--- jb_consul/definitions/fetch_instances_by_tag.rb
+++ jb_consul/definitions/fetch_instances_by_tag.rb
@@ -6,7 +6,7 @@ define :fetch_instances_by_tag, :tag_name => 'consul', :tag_value => 'bootstrap'
     ruby_block "get instances " + params[:tag_value] do
         block do
             ENV['HOME'] = node[:etc][:passwd][node['user']][:dir]
-            node.override['jb_consul']['instances'][params[:tag_value]] = JSON.parse(`aws --region #{region} --profile default ec2 describe-instances --filters 'Name=tag:#{params[:tag_name]},Values=#{params[:tag_value]}'` || '{"Reservations": []}')
+            node.override['jb_consul']['instances'][params[:tag_value]] = JSON.parse(`aws --region #{region} --profile default ec2 describe-instances --filters 'Name=tag:#{params[:tag_name]},Values=#{params[:tag_value]}'`)
         end
         action :run
     end
