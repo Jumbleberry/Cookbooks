b372a928618d11ad434d4df2a6d526cd17fb92f9
Ian Elliott <ian@elliian.com>
2014-12-30 16:08:44 -0500

    Add credentials to JBX Route

--- jbx/recipes/route.rb
+++ jbx/recipes/route.rb
@@ -23,6 +23,31 @@ template virtualhost do
   })
 end
 
+#Update application credentials
+credentials_file = "#{node['jbx']['route']['path']}/settings/databases.ini"
+credentials_file_template = "databases.ini.erb"
+
+template credentials_file do
+  source credentials_file_template
+  variables ({
+      "mysql_read_host"         => 'read1.mysql.jumbleberry.com',
+      "mysql_read_username"     => node['jbx']['credentials']['mysql_read']['username'],
+      "mysql_read_password"     => node['jbx']['credentials']['mysql_read']['password'],
+      "mysql_read_database"     => node['jbx']['credentials']['mysql_read']['dbname'],
+
+      "hitpath_host"            => node['jbx']['credentials']['hitpath']['host'],
+      "hitpath_username"        => node['jbx']['credentials']['hitpath']['username'],
+      "hitpath_password"        => node['jbx']['credentials']['hitpath']['password'],
+      "hitpath_database"        => node['jbx']['credentials']['hitpath']['dbname'],
+
+      "redis_host"              => 'read1.redis.jumbleberry.com',
+      "redis_port"              => node['jbx']['credentials']['redis']['port'],
+
+      "crypt"                   => node['jbx']['credentials']['crypt'],
+      "raygun"                  => node['jbx']['credentials']['raygun']
+    })
+end
+
 service 'nginx' do
   supports :status => true, :restart => true, :reload => true, :stop => true
   action :nothing
