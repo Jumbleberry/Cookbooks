3d641a45f2d623d0167848e7deae036209627e31
Ian Elliott <ian@elliian.com>
2015-01-06 11:42:24 -0500

    Inclusion of mocked_stack for vagrant

--- jb_consul/recipes/default.rb
+++ jb_consul/recipes/default.rb
@@ -1,5 +1,18 @@
-#Copy the cron script to the consul configuration folder
 cron_path = node["consul"]["config_dir"]
+
+# Copy the mocked_stack script for non-production environments
+ip = node[:network][:interfaces][node[:consul][:bind_interface]][:addresses].detect{|k,v| v[:family] == "inet" }.first
+template "#{cron_path}/mocked_stack.json" do
+    source "mocked_stack.json.erb"
+    owner "root"
+    group "root"
+    variables({
+        :ip          => ip,
+        :environment => node['environment']
+    })
+end
+
+#Copy the cron script to the consul configuration folder
 template "#{cron_path}/consul_cron.php" do
     source "consul_cron.php.erb"
     owner "root"
