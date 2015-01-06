8c8a66ccd1d3b3495d8d61b2ead0eec3ba2f3c89
Ian Elliott <ian@elliian.com>
2015-01-06 14:03:36 -0500

    Clear out consul configuration when deploying a consul node

--- jb_consul/recipes/default.rb
+++ jb_consul/recipes/default.rb
@@ -1,4 +1,45 @@
+include_recipe "aws"
+include_recipe "consul"
+
 cron_path = node["consul"]["config_dir"]
+
+# Turn off the existing consul service
+service "consul" do
+  supports :status => true, :restart => true, :reload => true, :stop => true
+  action :stop
+end
+
+# Remove consul files
+directory node['consul']['data_dir'] do
+  recursive true
+  action :delete
+end
+
+# Recreate the directory if necessary
+directory node['consul']['data_dir'] do
+  recursive true
+  owner 'root'
+  group 'root'
+  action :create
+end
+
+# Reset AWS Tags
+# If tag elements are present, and this is on EC2
+if ( node.attribute?('ec2') && node.attribute?('aws') && node['aws'].attribute?('tags') )
+    aws_resource_tag node['ec2']['instance_id'] do
+        aws_access_key node['aws']['aws_access_key_id']
+        aws_secret_access_key node['aws']['aws_secret_access_key']
+        tags({
+            "consul" => "bootstrap"
+        })
+        action :update
+    end
+end
+
+# Turn consul back on
+service "consul" do
+  action :start
+end
 
 # Copy the mocked_stack script for non-production environments
 ip = node[:network][:interfaces][node[:consul][:bind_interface]][:addresses].detect{|k,v| v[:family] == "inet" }.first
