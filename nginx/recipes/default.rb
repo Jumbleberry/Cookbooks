10089e978ca3355319487f613a32ecc0c4937583
Ian Elliott <ian@elliian.com>
2015-01-08 14:48:08 -0500

    Add an nginx default recipe for status pages

--- nginx/recipes/default.rb
+++ nginx/recipes/default.rb
@@ -19,6 +19,19 @@ link '/etc/nginx/sites-enabled/default' do
   action :delete
 end
 
+virtualhost         = '/etc/nginx/sites-available/default'
+virtualhost_link    = '/etc/nginx/sites-enabled/default'
+
+template virtualhost do
+  source    "nginx/default.erb"
+end
+
+link virtualhost_link do
+  to virtualhost
+  notifies :reload, "service[nginx]"
+end
+
+
 service 'nginx' do
   supports :status => true, :restart => true, :reload => true, :stop => true
   action [ :enable, :start ]
