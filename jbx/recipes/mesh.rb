3ce00bf3b39bb1a052abe2cb9dd711be7e405c6b
Ian Elliott <ian@elliian.com>
2015-01-13 14:40:45 -0500

    Dependencies already come from jbx::core

--- jbx/recipes/mesh.rb
+++ jbx/recipes/mesh.rb
@@ -1,9 +1,3 @@
-include_recipe "timezone-ii"
-include_recipe "web-server"
-include_recipe "github-auth"
-include_recipe "nginx"
-include_recipe "php"
-include_recipe "phalcon"
 include_recipe "jbx::core"
 
 # Set the branch to checkout
