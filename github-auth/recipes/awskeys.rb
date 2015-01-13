87bacc4170e44194e9627fb5e95b50a4f8f91cf5
Ian Elliott <ian@elliian.com>
2015-01-13 18:29:40 -0500

    Don't decrypt github keys if not necessary

--- github-auth/recipes/awskeys.rb
+++ github-auth/recipes/awskeys.rb
@@ -10,19 +10,21 @@ directory path do
   recursive true
 end
 
-node['github-auth']['keys'].each do | app, key |
-    #Move encrypted key file to destination folder
-    cookbook_file "#{path}/#{key}.key.gpg" do
-        source "ssh/#{key}.key.gpg"
-        owner "root"
-        group "root"
-        mode "0664"
-    end
-
-    #Unencrypt key using the previosuly imported gpg key
-    execute "unencrypt key: #{app}" do
-        cwd path
-        command "gpg --output #{key}.key --decrypt --batch --yes #{key}.key.gpg && chmod 0600 #{key}.key"
-        user "root"
+if !node['github-auth']['keys'].empty?
+    node['github-auth']['keys'].each do | key |
+        #Move encrypted key file to destination folder
+        cookbook_file "#{path}/#{key}.key.gpg" do
+            source "ssh/#{key}.key.gpg"
+            owner "root"
+            group "root"
+            mode "0664"
+        end
+    
+        #Unencrypt key using the previosuly imported gpg key
+        execute "unencrypt key: #{app}" do
+            cwd path
+            command "gpg --output #{key}.key --decrypt --batch --yes #{key}.key.gpg && chmod 0600 #{key}.key"
+            user "root"
+        end
     end
 end
