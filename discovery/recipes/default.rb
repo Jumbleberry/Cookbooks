24d42ed4b1d99ad9fe7c19dcf6bea16d1dae518e
vagrant <vagrant@jbx-web-server.dev.jumbleberry.com>
2015-01-19 15:27:50 -0500

    modified route53 for failover. removed route53 from berksfile. added DNS creation to discovery

--- discovery/recipes/default.rb
+++ discovery/recipes/default.rb
@@ -4,24 +4,42 @@ aws_resource_tag node['ec2']['instance_id'] do
       "Environment" => node.chef_environment
     })
   action :update
+
 end
 
 include_recipe "route53"
 
-node['aws']['route53']['entries'].each do | entry |
+node['aws']['route53']['entries'].each do | entry | 
+  
   route53_record "create a record" do
     name  entry.name
     type  entry.type
-    alias_target "dualstack." + entry.alias_target
-    zone_id               entry.zone_id
+    zone_id entry.zone_id
+    
+    if !entry['target'].nil?
+      aliasTarget = {
+        :hosted_zone_id         => entry.hosted_zone_id,
+        :dns_name               => entry.target,
+        :evaluate_target_health => entry.evaluate_target_health,
+      }         
+      
+      alias_target aliasTarget
+    else
+      value entry.value
+    end    
+    
+    if !entry['failover_record_type'].nil?
+      routingPolicy = {
+        :failover       => entry.failover_record_type,
+        :set_identifier => entry.failover_record_type + "-" + entry.name
+      }       
+      
+      routing_policy routingPolicy
+    end
+    
     aws_access_key_id     node['aws']['route53']['aws_access_key_id']
-    aws_secret_access_key node['aws']['route53']['aws_secret_access_key']
-    routing_policy entry.routing_policy
-    failover_record_type entry.failover_record_type
-    set_id entry.name + "-" + entry.failover_record_type
-    evaluate_target_health entry.evaluate_target_health
-    associate_with_health_check entry.associate_with_health_check
+    aws_secret_access_key node['aws']['route53']['aws_secret_access_key']            
     overwrite true
     action :create
-  end
+  end      
 end
