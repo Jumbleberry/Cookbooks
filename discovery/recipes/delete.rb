568f07910f3a47c951e533af8fae9f27d557d610
Dave Wong <dave.wong@jumbleberry.com>
2015-01-20 10:55:17 -0500

    changed the way IPs are added

--- discovery/recipes/delete.rb
+++ discovery/recipes/delete.rb
@@ -2,15 +2,4 @@ include_recipe "route53"
 
 node.override['aws']['route53']['instance_destroy'] = true;
 
-node['aws']['route53']['entries'].each do | entry |
-  route53_record "delete a record" do
-    name entry.name
-    type entry.type
-    zone_id entry.zone_id
-    aws_access_key_id     node['aws']['aws_access_key_id']
-    aws_secret_access_key node['aws']['aws_secret_access_key']
-    action :delete
-  end
-end
-
 include_recipe "discovery::add"
