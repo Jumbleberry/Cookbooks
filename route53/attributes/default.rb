aa1cee16bb999b5bff6441e85513086587056059
Dave Wong <dave.wong@jumbleberry.com>
2015-01-20 09:54:43 -0500

    added default IP

--- route53/attributes/default.rb
+++ route53/attributes/default.rb
@@ -2,3 +2,4 @@ default['build_essential']['compiletime'] = true
 default['build-essential']['compile_time'] = true
 default['route53']['nokogiri_version'] = '1.6.3.1'
 default['route53']['fog_version'] = '1.24'
+default['route53']['instanceIp'] = node[:network][:interfaces][node[:network_interface]][:addresses].detect{|k,v| v[:family] == "inet" }.first
