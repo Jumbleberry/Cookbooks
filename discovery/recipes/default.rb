123b920c4a477f078cbfd1f4b0f9d7757f6babba
Dave Wong <dave.wong@jumbleberry.com>
2015-01-20 13:29:50 -0500

    fixed filename for metadata

--- discovery/recipes/default.rb
+++
@@ -1,8 +0,0 @@
-aws_resource_tag node['ec2']['instance_id'] do
-  tags({
-      "Name"        => "OpsWorks Production Stack",
-      "Environment" => node.chef_environment
-    })
-  action :update
-
-end
