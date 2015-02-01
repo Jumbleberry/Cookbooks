21369228e2145e928c17c3ca337d0d2192dfa1b5
Ian Elliott <ian@elliian.com>
2015-02-01 14:30:23 -0500

    Gearman configuration for JBX, Gearman Manager and Consul

--- gearman/recipes/default.rb
+++ gearman/recipes/default.rb
@@ -1,3 +1,5 @@
+include_recipe "jb_consul"
+
 # Add ppa repository
 apt_repository 'gearman-developers' do
   uri           'ppa:gearman-developers/ppa'
@@ -38,7 +40,46 @@ execute "sudo killall gearmand" do
     user "root"
 end
 
-
 execute "sudo service gearman-job-server restart" do
+    user "root"
+end
+
+# Consul Integration
+consul_path = node["consul"]["config_dir"]
+cron_path   = node["consul"]["config_dir"]
+
+# Add the cron helper libraries to the consul config folder
+remote_directory "#{cron_path}/GearmanAdmin" do
+    source "GearmanAdmin"
+    files_mode "0664"
+    owner "root"
+    group "root"
+    mode "0775"
+end
+
+# Move the health check file
+cookbook_file "gearman_check.php" do
+    path "#{cron_path}/gearman_check.php"
+    owner "root"
+    group "cluster"
+    mode "0755"
+end
+
+#Creates the service configuration file
+template "#{consul_path}/gearman.json" do
+    source "gearman_service.json.erb"
+    owner "root"
+    group "cluster"
+    mode "0664"
+    variables ({
+        "currentip" => node["gearman"]["ip"],
+        "consul_path" => consul_path
+    })
+end
+
+# Reload consul
+execute "reload consul configuration" do
+    command "consul reload || true"
+    ignore_failure true
     user "root"
 end
