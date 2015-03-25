296ab0fe5771b64f29dbcb566f1f4e1f90ea2ced
Ian Elliott <ian@elliian.com>
2015-03-25 12:06:11 -0400

    Processing server doesn't need to be reachable via url

--- jbx/recipes/processing.rb
+++ jbx/recipes/processing.rb
@@ -20,25 +20,25 @@ execute "chown-data-www" do
 end
 
 # Creates the nginx virtual host
-virtualhost         = '/etc/nginx/sites-available/' + node['jbx']['processing']['hostname']
-virtualhost_link    = '/etc/nginx/sites-enabled/' + node['jbx']['processing']['hostname']
-
-template virtualhost do
-  source    "nginx/processing.erb"
-  variables ({
-    "hostname"  => node['jbx']['processing']['hostname'],
-    "path"      => "#{node['jbx']['core']['path']}/public"
-  })
-end
-service 'nginx' do
-  supports :status => true, :restart => true, :reload => true, :stop => true
-  action :nothing
-end
-
-link virtualhost_link do
-  to virtualhost
-  notifies :reload, "service[nginx]"
-end
+# virtualhost         = '/etc/nginx/sites-available/' + node['jbx']['processing']['hostname']
+# virtualhost_link    = '/etc/nginx/sites-enabled/' + node['jbx']['processing']['hostname']
+# 
+# template virtualhost do
+#   source    "nginx/processing.erb"
+#   variables ({
+#     "hostname"  => node['jbx']['processing']['hostname'],
+#     "path"      => "#{node['jbx']['core']['path']}/public"
+#   })
+# end
+# service 'nginx' do
+#   supports :status => true, :restart => true, :reload => true, :stop => true
+#   action :nothing
+# end
+# 
+# link virtualhost_link do
+#   to virtualhost
+#   notifies :reload, "service[nginx]"
+# end
 
 # Un-symlink our config script to prevent install file from overwriting our good version
 link "/etc/gearman-manager/config.ini" do
