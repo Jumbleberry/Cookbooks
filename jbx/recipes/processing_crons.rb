b32eb1ccb26321a208ba172a5a6fd8a38a77a6ff
Ian Elliott <ian@elliian.com>
2015-03-26 15:59:08 -0400

    Update cron run times

--- jbx/recipes/processing_crons.rb
+++ jbx/recipes/processing_crons.rb
@@ -1,27 +1,30 @@
 cron "Processing - Detect CRM Features" do
   command "/usr/bin/php #{node[:jbx][:processing][:path]}/crons/detect_features.php"
   hour '4'
+  minute '30'
   user 'www-data'
   action :delete
 end
 
 cron "Processing - Get CRM Campaigns" do
   command "/usr/bin/php #{node[:jbx][:processing][:path]}/crons/get_campaigns.php"
-  minute '45'
+  hour '2'
+  minute '30'
   user 'www-data'
   action :delete
 end
 
-cron "Processing - Get CRM Daily Stats" do
-  command "/usr/bin/php #{node[:jbx][:processing][:path]}/crons/get_daily_stats.php"
+cron "Processing - Get CRM Current Day Stats" do
+  command "/usr/bin/php #{node[:jbx][:processing][:path]}/crons/get_current_stats.php"
   minute '5'
   user 'www-data'
   action :delete
 end
 
-cron "Processing - Get CRM Weekly Stats" do
-  command "/usr/bin/php #{node[:jbx][:processing][:path]}/crons/get_weekly_stats.php"
-  hour '2,4,6'
+cron "Processing - Get CRM Historical Stats" do
+  command "/usr/bin/php #{node[:jbx][:processing][:path]}/crons/get_historical_stats.php"
+  hour '5'
+  minute '30'
   user 'www-data'
   action :delete
 end
