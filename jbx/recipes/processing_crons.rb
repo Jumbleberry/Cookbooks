b43b2ae4085c5c1a90534c47df80920abd91a9c9
Ian Elliott <ian@elliian.com>
2015-03-26 08:12:17 -0400

    Disable crons for now

--- jbx/recipes/processing_crons.rb
+++ jbx/recipes/processing_crons.rb
@@ -2,26 +2,26 @@ cron "Processing - Detect CRM Features" do
   command "/usr/bin/php #{node[:jbx][:processing][:path]}/crons/detect_features.php"
   hour '4'
   user 'www-data'
-  action :create
+  action :delete
 end
 
 cron "Processing - Get CRM Campaigns" do
   command "/usr/bin/php #{node[:jbx][:processing][:path]}/crons/get_campaigns.php"
   minute '45'
   user 'www-data'
-  action :create
+  action :delete
 end
 
 cron "Processing - Get CRM Daily Stats" do
   command "/usr/bin/php #{node[:jbx][:processing][:path]}/crons/get_daily_stats.php"
   minute '5'
   user 'www-data'
-  action :create
+  action :delete
 end
 
 cron "Processing - Get CRM Weekly Stats" do
   command "/usr/bin/php #{node[:jbx][:processing][:path]}/crons/get_weekly_stats.php"
   hour '2,4,6'
   user 'www-data'
-  action :create
+  action :delete
 end
