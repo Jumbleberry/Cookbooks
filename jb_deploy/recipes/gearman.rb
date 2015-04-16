a7887d939dd1b7c961fbc1f17de40b3e1b5fd2aa
Ian Elliott <ian@elliian.com>
2015-04-16 11:25:48 -0400

    Only reboot gearman server when required

--- jb_deploy/recipes/gearman.rb
+++ jb_deploy/recipes/gearman.rb
@@ -1,14 +1,14 @@
 #Update configuration file
-template '/etc/default/gearman-job-server' do
+server = template '/etc/default/gearman-job-server' do
     source 'gearman-job-server.erb'
     owner 'root'
     group 'root'
     mode '0644'
-    notifies :run, 'execute[gearman-restart]', :immediately
 end
 
 execute "gearman-restart" do
     command "sudo service gearman-job-server restart"
     user "root"
-    action :nothing
+    action :run
+    only_if { server.updated_by_last_action? }
 end
