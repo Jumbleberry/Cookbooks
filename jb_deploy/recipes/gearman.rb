8a6a41540cc15594660741dcbe1a66335642dc52
Ian Elliott <ian@elliian.com>
2015-04-16 17:22:15 -0400

    Change how gearman-restart gets invoked

--- jb_deploy/recipes/gearman.rb
+++ jb_deploy/recipes/gearman.rb
@@ -1,15 +1,15 @@
+# Force Kill old gearman-manager processes and restart the job server
+execute "gearman-restart" do
+    command "pkill -9 -u `id -u gearman` && sudo service gearman-job-server restart"
+    user "root"
+    action :nothing
+end
+
 #Update configuration file
 server = template '/etc/default/gearman-job-server' do
     source 'gearman-job-server.erb'
     owner 'root'
     group 'root'
     mode '0644'
-end
-
-# Force Kill old gearman-manager processes and restart the job server
-execute "gearman-restart" do
-    command "pkill -9 -u `id -u gearman` && sudo service gearman-job-server restart"
-    user "root"
-    action :run
-    only_if { server.updated_by_last_action? }
+    notifies :run, 'execute[gearman-restart]', :immediately
 end
