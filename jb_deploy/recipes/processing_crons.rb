3141a6ad364968f26602dcfb53b985ac7d7887ab
Ian Elliott <ian@elliian.com>
2015-04-21 01:36:20 -0400

    Add Crons

--- jb_deploy/recipes/processing_crons.rb
+++ jb_deploy/recipes/processing_crons.rb
@@ -1,30 +1,31 @@
 cron "Processing - Detect CRM Features" do
   command "/usr/bin/php #{node[:jbx][:processing][:path]}/crons/detect_features.php"
   hour '4'
-  minute '30'
+  minute '45'
   user 'www-data'
-  action :delete
+  action :create
 end
 
 cron "Processing - Get CRM Campaigns" do
   command "/usr/bin/php #{node[:jbx][:processing][:path]}/crons/get_campaigns.php"
-  hour '2'
+  hour '2-24/4'
   minute '30'
   user 'www-data'
-  action :delete
+  action :create
 end
 
 cron "Processing - Get CRM Current Day Stats" do
   command "/usr/bin/php #{node[:jbx][:processing][:path]}/crons/get_current_stats.php"
+  hour '0-2,4,6,8-24'
   minute '0'
   user 'www-data'
-  action :delete
+  action :create
 end
 
 cron "Processing - Get CRM Historical Stats" do
   command "/usr/bin/php #{node[:jbx][:processing][:path]}/crons/get_historical_stats.php"
-  hour '0'
+  hour '3,5,7'
   minute '30'
   user 'www-data'
-  action :delete
+  action :create
 end
