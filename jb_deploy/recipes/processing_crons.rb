847764c1672d1b924fca9bf9fb0de6289932e055
Ian Elliott <ian@elliian.com>
2015-04-28 16:07:22 -0400

    modify cron times

--- jb_deploy/recipes/processing_crons.rb
+++ jb_deploy/recipes/processing_crons.rb
@@ -16,15 +16,15 @@ end
 
 cron "Processing - Get CRM Current Day Stats" do
   command "/usr/bin/php #{node[:jbx][:processing][:path]}/crons/get_current_stats.php"
-  hour '0-2,4,6,8-23'
+  hour '0-2,6,8-23'
   minute '15'
   user 'www-data'
-  action :delete
+  action :create
 end
 
 cron "Processing - Get CRM Historical Stats" do
   command "/usr/bin/php #{node[:jbx][:processing][:path]}/crons/get_historical_stats.php"
-  hour '3,5,7'
+  hour '3,5'
   minute '15'
   user 'www-data'
   action :create
