ba4b919452ab7e5f4d510c8c817e050acbf557bb
Ian Elliott <ian@elliian.com>
2015-05-05 01:19:43 -0400

    fix crons

--- jb_deploy/recipes/mail_crons.rb
+++ jb_deploy/recipes/mail_crons.rb
@@ -2,7 +2,7 @@ cron "Mail - Credential Status Notice" do
   command "/usr/bin/php #{node[:jbx][:mail][:path]}/crons/credential_status.php"
   hour '9'
   minute '0'
-  weekday :tuesday
+  weekday 2
   user 'www-data'
   action :create
 end
@@ -11,7 +11,7 @@ cron "Mail - Credential Campaigns Notice" do
   command "/usr/bin/php #{node[:jbx][:mail][:path]}/crons/credential_campaigns.php"
   hour '9'
   minute '0'
-  weekday :tuesday
+  weekday 2
   user 'www-data'
   action :create
 end
