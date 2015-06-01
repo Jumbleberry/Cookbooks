16a75ac3560e5ce00a4da09b0f3d5e39cedf4a9f
Ian Elliott <ian@elliian.com>
2015-06-01 14:01:15 -0400

    add order crons

--- jb_deploy/recipes/processing_crons.rb
+++ jb_deploy/recipes/processing_crons.rb
@@ -1,7 +1,7 @@
 cron "Processing - Detect CRM Features" do
   command "/usr/bin/php #{node[:jbx][:processing][:path]}/crons/detect_features.php"
   hour '4'
-  minute '0'
+  minute '20'
   user 'www-data'
   action :create
 end
@@ -25,6 +25,22 @@ end
 cron "Processing - Get CRM Historical Stats" do
   command "/usr/bin/php #{node[:jbx][:processing][:path]}/crons/get_historical_stats.php"
   hour '3,5'
+  minute '20'
+  user 'www-data'
+  action :create
+end
+
+cron "Processing - Get CRM Current Orders" do
+  command "/usr/bin/php #{node[:jbx][:processing][:path]}/crons/get_current_orders.php"
+  hour '0-2,7-23'
+  minute '5-59/10'
+  user 'www-data'
+  action :create
+end
+
+cron "Processing - Get CRM Historical Orders" do
+  command "/usr/bin/php #{node[:jbx][:processing][:path]}/crons/get_historical_orders.php"
+  hour '4'
   minute '20'
   user 'www-data'
   action :create
