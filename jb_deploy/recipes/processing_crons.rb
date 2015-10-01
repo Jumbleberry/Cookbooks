a9e4a62bce8388deeb63c567fa2c32b0c8ce3f20
Ian Elliott <ian@elliian.com>
2015-10-01 17:15:58 -0400

    retentions at 11:15pm

--- jb_deploy/recipes/processing_crons.rb
+++ jb_deploy/recipes/processing_crons.rb
@@ -62,7 +62,7 @@ end
 
 cron "Processing - Calculate Retentions" do
   command "/usr/bin/php #{node[:jbx][:processing][:path]}/crons/calculate_retentions.php"
-  hour '6'
+  hour '23'
   minute '15'
   user 'www-data'
   action :create
