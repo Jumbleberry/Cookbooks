26ff8e2e7956ae58426cabcb94e2e54ce450f02b
obiwongee <dave.wong@jumbleberry.com>
2015-10-06 15:59:42 -0400

    calculate retentions ever hour

--- jb_deploy/recipes/processing_crons.rb
+++ jb_deploy/recipes/processing_crons.rb
@@ -62,7 +62,7 @@ end
 
 cron "Processing - Calculate Retentions" do
   command "/usr/bin/php #{node[:jbx][:processing][:path]}/crons/calculate_retentions.php"
-  hour '23'
+  hour '*'
   minute '15'
   user 'www-data'
   action :create
