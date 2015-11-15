cf32a15c5fedf7c03162dd5879d870a055fa394f
Ian Elliott <ian@elliian.com>
2015-11-15 03:41:27 -0500

    fix cert typo

--- nginx/recipes/certs.rb
+++ nginx/recipes/certs.rb
@@ -12,21 +12,21 @@ node['nginx']['certs'].each do | cert |
     [ "key", "pem" ].each do | ext |
 
         #Move encrypted key file to destination folder
-        if File.exist?("ssl/#{cert}.#{ext}#{gpg}")
-            cookbook_file "/etc/nginx/ssl/#{r}.#{ext}#{gpg}" do
-                source "ssl/#{cert}.#{ext}#{gpg}"
-                owner "root"
-                group "root"
-                mode "0664"
-            end
-        
-            if node.attribute?('aws_deploy_key')
-                #Unencrypt key using the previosuly imported gpg key
-                execute "unencrypt key: #{cert}.#{ext}" do
-                    cwd '/etc/nginx/ssl/'
-                    command "gpg --output #{cert}.#{ext} --decrypt --batch --yes #{cert}.#{ext}.gpg && chmod 0600 #{cert}.#{ext}"
-                    user "root"
-                end
+        cookbook_file "/etc/nginx/ssl/#{cert}.#{ext}#{gpg}" do
+            source "ssl/#{cert}.#{ext}#{gpg}"
+            owner "root"
+            group "root"
+            mode "0664"
+            ignore_failure true
+        end
+    
+        if node.attribute?('aws_deploy_key')
+            #Unencrypt key using the previosuly imported gpg key
+            execute "unencrypt key: #{cert}.#{ext}" do
+                cwd '/etc/nginx/ssl/'
+                command "gpg --output #{cert}.#{ext} --decrypt --batch --yes #{cert}.#{ext}.gpg && chmod 0600 #{cert}.#{ext}"
+                only_if { File.exists?("/etc/nginx/ssl/#{cert}.#{ext}#{gpg}") }
+                user "root"
             end
         end
     end
