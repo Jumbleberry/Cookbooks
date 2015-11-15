27be48cbb4981bf8e5cb23e8643c5625c7df4161
Ian Elliott <ian@elliian.com>
2015-11-15 02:07:43 -0500

    ensure gearman server is running

--- jb_deploy/recipes/gearman.rb
+++ jb_deploy/recipes/gearman.rb
@@ -1,6 +1,6 @@
 # Force Kill old gearman-manager processes and restart the job server
 execute "gearman-restart" do
-    command "pkill -9 -u `id -u gearman`; sudo service gearman-job-server restart"
+    command "pkill -9 -u `id -u gearman`; service gearman-job-server restart"
     user "root"
     action :nothing
 end
@@ -12,4 +12,15 @@ server = template '/etc/default/gearman-job-server' do
     group 'root'
     mode '0644'
     notifies :run, 'execute[gearman-restart]', :immediately
+end
+
+service "gearman-job-server" do
+  supports :status => true
+  action :start
+end
+
+execute "gearman-job-server start" do
+    command "service gearman-job-server start"
+    user "root"
+    ignore_failure true
 end
