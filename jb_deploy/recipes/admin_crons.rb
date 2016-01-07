5465201a72c4ff54e5eff611397ae91d405098eb
Ian Elliott <ian@elliian.com>
2016-01-07 14:49:28 -0500

    disable cron

--- jb_deploy/recipes/admin_crons.rb
+++ jb_deploy/recipes/admin_crons.rb
@@ -24,7 +24,7 @@ cron "Admin - Hitpath Raw Logs" do
   command "/usr/bin/php #{node[:admin][:path]}/cron_scripts/raw_log_alerts.php"
   minute '*/5'
   user 'www-data'
-  action :create
+  action :delete
 end
 
 
