ebe792c6eb069637c36bb8d3fede616a9d35b590
Ian Elliott <ian@elliian.com>
2016-01-07 16:14:18 -0500

    disable route crons

--- jb_deploy/recipes/route_crons.rb
+++ jb_deploy/recipes/route_crons.rb
@@ -3,12 +3,12 @@ cron "Route - Sync Sales to Redis" do
   command "/usr/bin/php #{node['jbx']['route']['path']}/crons/sales.php"
   minute '*'
   user 'www-data'
-  action :create
+  action :delete
 end
 
 cron "Route - Sync Redirects to Redis" do
   command "/usr/bin/php #{node['jbx']['route']['path']}/crons/sync.php"
   minute '*/5'
   user 'www-data'
-  action :create
+  action :delete
 end
