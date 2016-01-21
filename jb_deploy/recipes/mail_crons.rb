bbf01ea67f4bb43e0aa5a2bc14daedab26f75843
Hao <hao.ling@jumbleberry.com>
2016-01-21 11:50:41 -0500

    Change cap approval notice mail cron to Friday 4:10pm

--- jb_deploy/recipes/mail_crons.rb
+++ jb_deploy/recipes/mail_crons.rb
@@ -18,9 +18,9 @@ end
 
 cron "Mail - CAP Approvals Notices" do
   command "/usr/bin/php #{node[:jbx][:mail][:path]}/crons/cap_approval.php"
-  hour '8,16'
+  hour '16'
   minute '10'
-  weekday 1
+  weekday 5
   user 'www-data'
   action :create
 end
