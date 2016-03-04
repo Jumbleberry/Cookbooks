b621a8cde7a66d614c49167c4d47cd3709a6ea46
obiwongee <dave.wong@jumbleberry.com>
2016-03-04 14:32:01 -0500

    alchemy keys

--- jb_deploy/recipes/core.rb
+++ jb_deploy/recipes/core.rb
@@ -70,7 +70,10 @@ template credentials_file do
       
       "webdriver_user"          => node['web_driver']['user'],
       "webdriver_password"      => node['web_driver']['password'],
-      "webdriver_url"           => node['web_driver']['url']
+      "webdriver_url"           => node['web_driver']['url'],
+
+      "alchemy_free"            => node['alchemy']['free'],
+      "alchemy_paid"            => node['alchemy']['paid']
     })
 end
 
