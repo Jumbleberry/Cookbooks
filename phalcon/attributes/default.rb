ea63ff05feceec3b8382201e8ef6f8e52727faf3
obiwongee <dave.wong@jumbleberry.com>
2016-03-15 15:57:35 -0400

    hosted consul on S3 initial change for Phalcon2

--- phalcon/attributes/default.rb
+++ phalcon/attributes/default.rb
@@ -1,8 +1,11 @@
 default['phalcon']['git_url'] = 'git://github.com/phalcon/cphalcon.git'
-default['phalcon']['git_ref'] = '1.3.4'
+default['phalcon']['revision'] = 'phalcon-v2.0.10'
 default['phalcon']['download_path'] = "/tmp/cphalcon"
 default['phalcon']['build_path'] = "build"
 default['phalcon']['conf_dirs'] = ['/etc/php5/mods-available']
 default['phalcon']['conf_dirs_alias'] = ['/etc/php5/cli/conf.d', '/etc/php5/fpm/conf.d']
 default['phalcon']['conf_file'] = '20-phalcon.ini'
 default['phalcon']['devtools'] = false
+
+default['zephir']['git_url'] = 'git@github.com:phalcon/zephir.git'
+default['zephir']['revision'] = '0.8.0'
