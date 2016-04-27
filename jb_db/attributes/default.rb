11ab12da4eca100648c502f3d22c01853a76fa13
obiwongee <dave.wong@jumbleberry.com>
2016-04-27 15:08:04 -0400

    disable awscli compile_time

--- jb_db/attributes/default.rb
+++ jb_db/attributes/default.rb
@@ -3,6 +3,7 @@ default['jbdb_importer']['bin_directory']    = '/usr/bin'
 
 default['mysql']['server_root_user']     = 'root'
 default['mysql']['server_root_password'] = node['jbx']['credentials']['mysql_read']['password']
-default['mysql']['database']             = 'jbx'
+default['mysql']['database']             = node['jbx']['credentials']['mysql_read']['dbname']
 
 default['aws']['db_bucket'] = 's3://miscfile-staging/db-backups'
+default[:aws][:compile_time] = false
