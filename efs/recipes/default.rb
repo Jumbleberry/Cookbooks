d3ff7921a62b4945aa570012ade1420077c70ab8
Hao-Jumbleberry <hao.ling@jumbleberry.com>
2016-08-23 18:09:43 -0400

    Create EFS mount point before mounting the EFS

--- efs/recipes/default.rb
+++ efs/recipes/default.rb
@@ -3,9 +3,17 @@ package "#nfs-common" do
     action :install
 end
 
+# Create the EFS mount point
+efsMountPoint = '/html'
+directory efsMountPoint do
+    user 'www-data'
+    group 'www-data'
+    mode '0444'
+    action :create
+end
+
 # Mount EFS
 mountTargetDNS = 'us-east-1c.fs-8ac002c3.efs.us-east-1.amazonaws.com'
-efsMountPoint = '/public'
 execute "mount-efs" do
   command "mount -t nfs4 -o vers=4.1 {mountTargetDNS}:/ {efsMountPoint}"
   user 'root'
