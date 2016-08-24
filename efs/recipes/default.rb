dcfef57904bffe45fe68c6ef47a22ef4c359b18d
Hao-Jumbleberry <hao.ling@jumbleberry.com>
2016-08-24 16:29:03 -0400

    Finish efs recipe

--- efs/recipes/default.rb
+++ efs/recipes/default.rb
@@ -5,18 +5,28 @@ package "nfs-common" do
     action :install
 end
 
+# Get AWS region
+execute "get-aws-region" do
+    command "awsRegion=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | grep region | awk -F\" '{print $4}')"
+end
+
+# Get the file system ID
+efsName = 'Campaigns'
+execute "get-file-system-id" do
+    command "fileSystemId=$(aws efs --region $awsRegion describe-file-systems | grep -iC 5 '\"Name\":\s\"{efsName}\"' | grep -ioP '(?<=FileSystemId\":\s\")[^\"]*' )"
+end
+
 # Create the EFS mount point
-# efsMountPoint = '/html'
-# directory efsMountPoint do
-#     user 'www-data'
-#     group 'www-data'
-#     mode '0444'
-#     action :create
-# end
+efsMountPoint = '/html'
+directory efsMountPoint do
+    user 'www-data'
+    group 'www-data'
+    mode '0444'
+    action :create
+end
 
 # Mount EFS
-# mountTargetDNS = 'us-east-1c.fs-8ac002c3.efs.us-east-1.amazonaws.com'
-# execute "mount-efs" do
-#   command "mount -t nfs4 -o vers=4.1 {mountTargetDNS}:/ {efsMountPoint}"
-#   user 'root'
-# end
+execute "mount-efs" do
+  command "mount -t nfs4 -o vers=4.1 $(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone).$fileSystemId.efs.$awsRegion.amazonaws.com:/ {efsMountPoint}"
+  user 'root'
+end
