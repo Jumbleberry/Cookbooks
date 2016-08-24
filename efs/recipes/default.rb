3f8d2b1d20097bcc00063569a5421d5d06bd2cad
Hao-Jumbleberry <hao.ling@jumbleberry.com>
2016-08-24 16:57:57 -0400

    Merge multiple commands on efs recipe

--- efs/recipes/default.rb
+++ efs/recipes/default.rb
@@ -5,17 +5,6 @@ package "nfs-common" do
     action :install
 end
 
-# Get AWS region
-execute "get-aws-region" do
-    command "awsRegion=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | grep region | awk -F\" '{print $4}')"
-end
-
-# Get the file system ID
-efsName = 'Campaigns'
-execute "get-file-system-id" do
-    command "fileSystemId=$(aws efs --region $awsRegion describe-file-systems | grep -iC 5 '\"Name\":\s\"{efsName}\"' | grep -ioP '(?<=FileSystemId\":\s\")[^\"]*' )"
-end
-
 # Create the EFS mount point
 efsMountPoint = '/html'
 directory efsMountPoint do
@@ -26,7 +15,15 @@ directory efsMountPoint do
 end
 
 # Mount EFS
-execute "mount-efs" do
-  command "mount -t nfs4 -o vers=4.1 $(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone).$fileSystemId.efs.$awsRegion.amazonaws.com:/ {efsMountPoint}"
-  user 'root'
+execute "get-aws-region" do
+    environment => {
+      'efsName' => 'Campaigns',
+      'efsMountPoint' => efsMountPoint
+    }
+    command <<-EOH
+        awsRegion=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | grep region | awk -F\" '{print $4}')
+        fileSystemId=$(aws efs --region $awsRegion describe-file-systems | grep -iC 5 '\"Name\":\s\"${efsName}\"' | grep -ioP '(?<=FileSystemId\":\s\")[^\"]*' )
+        mount -t nfs4 -o vers=4.1 $(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone).$fileSystemId.efs.$awsRegion.amazonaws.com:/ ${efsMountPoint}
+    EOH
+    user 'root'
 end
