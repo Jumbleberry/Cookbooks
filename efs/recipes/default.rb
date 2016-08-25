946bb13e030da3ef05724b31f1593f52fbcc695a
Hao-Jumbleberry <hao.ling@jumbleberry.com>
2016-08-25 10:04:28 -0400

    Start debugging line by line

--- efs/recipes/default.rb
+++ efs/recipes/default.rb
@@ -22,8 +22,6 @@ execute "get-aws-region" do
     })
     command <<-EOF
         awsRegion=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | grep region | awk -F\" '{print $4}')
-        fileSystemId=$(aws efs --region $awsRegion describe-file-systems | grep -iC 5 ${efsNameRegex} | grep -ioP '(?<=FileSystemId\":\s\")[^\"]*' )
-        mount -t nfs4 -o vers=4.1 $(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone).$fileSystemId.efs.$awsRegion.amazonaws.com:/ ${efsMountPoint}
         EOF
     user 'root'
 end
