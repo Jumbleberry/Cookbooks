cc982384826895c8b78473a4fe71cba426b69bcc
Hao-Jumbleberry <hao.ling@jumbleberry.com>
2016-08-25 11:08:13 -0400

    Add more attributes

--- efs/attributes/default.rb
+++ efs/attributes/default.rb
@@ -1,1 +1,8 @@
-default['region_regex'] = "(?<=region\" : \")[^\"]*"
+# System
+default['instance_identity_url'] = 'http://169.254.169.254/latest/dynamic/instance-identity/document'
+default['availability_zone_url'] = 'http://169.254.169.254/latest/meta-data/placement/availability-zone'
+# Config
+default['efs_mount_point'] = '/html'
+
+# Regex
+default['region_regex'] = '(?<=region\" : \")[^\"]*'
