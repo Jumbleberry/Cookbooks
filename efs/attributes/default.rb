fd17e7d15a3984d423c2678d46ff1f85e8f053e9
Hao-Jumbleberry <hao.ling@jumbleberry.com>
2016-08-25 13:54:22 -0400

    Clean up

--- efs/attributes/default.rb
+++ efs/attributes/default.rb
@@ -2,14 +2,6 @@
 default['instance_identity_url'] = 'http://169.254.169.254/latest/dynamic/instance-identity/document'
 default['availability_zone_url'] = 'http://169.254.169.254/latest/meta-data/placement/availability-zone'
 
-# Packages
-default['efs']['packages'] = [
-  { 
-      "name" => "nfs-common",
-      "version" => "*"
-  }
-]
-
 # Config
 default['efs_mount_point'] = '/html'
 
@@ -17,3 +9,11 @@ default['efs_mount_point'] = '/html'
 default['region_reg'] = '(?<=region\"\s:\s\")[^\"]*'
 default['efs_name_reg'] = '\"Name\":\s\"Campaigns\"'
 default['file_system_id_reg'] = '(?<=FileSystemId\":\s\")[^\"]*'
+
+# Packages
+default['efs']['packages'] = [
+  { 
+      "name" => "nfs-common",
+      "version" => "*"
+  }
+]
