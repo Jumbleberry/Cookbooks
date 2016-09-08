cf05f109a9071c6ec5d988342e64753159e0df27
Hao-Jumbleberry <hao.ling@jumbleberry.com>
2016-09-08 13:44:45 -0400

    Add letencrypt-aws environment variable

--- ssl/attributes/default.rb
+++ ssl/attributes/default.rb
@@ -1,6 +1,10 @@
 # Packages
 default['ssl']['packages'] = [
   { 
+      "name" => "build-essential",
+      "version" => "*"
+  },
+  { 
       "name" => "python-dev",
       "version" => "*"
   },
@@ -11,8 +15,25 @@ default['ssl']['packages'] = [
   { 
       "name" => "libssl-dev",
       "version" => "*"
+  },
+  { 
+      "name" => "git",
+      "version" => "*"
   }
 ]
 
-# letsencrypt-aws config
-default['letsencrypt_aws_github_url'] = 'https://github.com/alex/letsencrypt-aws.git'
+# Letsencrypt-aws config
+default['letsencrypt_aws']['github_url'] = 'https://github.com/alex/letsencrypt-aws.git'
+default['letsencrypt_aws']['repo_path'] = '/var/www'
+default['letsencrypt_aws']['repo_name'] = 'letsencrypt-aws'
+
+# ACME config - staging
+default['acme']['account_key']['staging'] = 'file:///var/www/letsencrypt-aws/acme-staging-private.pem'
+default['acme']['directory_url']['staging'] = 'https://acme-staging.api.letsencrypt.org/directory'
+# ACME config - production
+default['acme']['account_key']['production'] = 'file:///var/www/letsencrypt-aws/acme-production-private.pem'
+default['acme']['directory_url']['production'] = 'https://acme-v01.api.letsencrypt.org/directory'
+
+# ElB config
+default['elb']['name'] = 'secure-elb'
+default['elb']['hosts'] = ["jbxstatic.com", "jbxswarm.com", "www.jbxstatic.com", "www.jbxswarm.com"]
--- ssl/recipes/default.rb
+++ ssl/recipes/default.rb
@@ -1,14 +1,6 @@
 include_recipe "apt"
 include_recipe "awscli"
 
-# System packages
-syspackages = ['git', 'gcc', 'vim', 'libpcre3-dev', 'make', 'curl', 'unzip', 'uuid']
-syspackages.each do |pkg|
-  package "#{pkg}" do
-    action :install
-  end
-end
-
 # Install packages
 packages = node['ssl']['packages']
 packages.each do |pkg|
@@ -25,16 +17,18 @@ execute "pip-upgrade-requests" do
 end
 
 # Pull letsencrypt-aws repo from Github
-git "#{Chef::Config[:file_cache_path]}" do
-  repository node['letsencrypt_aws_github_url']
+git node['letsencrypt_aws']['repo_path'] do
+  repository node['letsencrypt_aws']['github_url']
   revision 'master'
   action :sync
 end
 
 # Install letencrypt-aws
 execute "install-letencrypt-aws" do
-    cwd "#{Chef::Config[:file_cache_path]}/letsencrypt-aws"
+    cwd "#{node['letsencrypt_aws']['repo_path']}/#{node['letsencrypt_aws']['repo_name']}"
     command <<-EOF
+        python -m pip install virtualenv
+        python -m virtualenv .venv
         pip install -U pip
         pip install flake8
         pip install -r requirements.txt
@@ -51,12 +45,32 @@ end
 
 # Make sure we have private key for ACME server
 # Make sure AWS credentials are configured (aws_access_key_id, aws_secret_access_key and aws_region)
+
 # Set environment variables for letsencrypt-aws
+execute "export-letsencrypt-aws-config" do
+    command <<-EOF
+        export LETSENCRYPT_AWS_CONFIG='{
+            "domains": [
+                {
+                    "elb": {
+                        "name": "#{node['elb']['name']}",
+                        "port": "443"
+                    },
+                    "hosts": #{node['elb']['hosts']},
+                    "key_type": "rsa"
+                }
+            ],
+            "acme_account_key": "#{node['acme']['account_key']['staging']}",
+            "acme_directory_url": "#{node['acme']['directory_url']['staging']}"
+        }'
+        EOF
+    user 'root'
+end
  
 # Run letsencrypt-aws
 # If the certificate is not expiring soon, but you need to issue a new one anyways, the --force-issue flag can be provided
 execute "run-letsencrypt-aws" do
-    cwd "#{Chef::Config[:file_cache_path]}/letsencrypt-aws"
+    cwd "#{node['letsencrypt_aws']['repo_path']}/#{node['letsencrypt_aws']['repo_name']}"
     command 'python letsencrypt-aws.py update-certificates --force-issue'
     user 'root'
 end
