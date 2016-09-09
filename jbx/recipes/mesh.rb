9f6f11ea9fe90d74adb295ffe231d7f383669d20
Bellardia <ian@elliian.com>
2016-09-09 16:10:28 -0400

    updates to mesh nginx templates

--- jbx/recipes/mesh.rb
+++ jbx/recipes/mesh.rb
@@ -5,20 +5,21 @@ include_recipe "jb_deploy::mesh"
 virtualhost         = '/etc/nginx/sites-available/' + node['jbx']['mesh']['hostname']
 virtualhost_link    = '/etc/nginx/sites-enabled/' + node['jbx']['mesh']['hostname']
 
+service 'nginx' do
+  supports :status => true, :restart => true, :reload => true, :stop => true
+  action :nothing
+end
+
 template virtualhost do
   source    "nginx/mesh.erb"
   variables ({
     "hostname"  => node['jbx']['mesh']['hostname'],
     "path"      => "#{node['jbx']['core']['path']}/public"
   })
-end
-
-service 'nginx' do
-  supports :status => true, :restart => true, :reload => true, :stop => true
-  action :nothing
+  notifies :reload, "service[nginx]", :delayed
 end
 
 link virtualhost_link do
   to virtualhost
-  notifies :reload, "service[nginx]"
+  notifies :reload, "service[nginx]", :delayed
 end
