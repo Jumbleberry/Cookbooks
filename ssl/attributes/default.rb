179e76c408a47c0b75ab4ac207f6656cbfbb92d4
Hao-Jumbleberry <hao.ling@jumbleberry.com>
2016-09-16 14:58:36 -0400

    Hard code SSL elb name for now

--- ssl/attributes/default.rb
+++ ssl/attributes/default.rb
@@ -32,7 +32,7 @@ default['letsencrypt_aws']['config'] = '{
     "domains": [
         {
             "elb": {
-                "name": "secure-elb",
+                "name": "us-east-1-NginxELB-1RFH6W6UZV9T9",
                 "port": "443"
             },
             "hosts": ["jbxstatic.com", "jbxswarm.com", "www.jbxstatic.com", "www.jbxswarm.com"],
