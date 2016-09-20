a0b2ce50aea723a75de3261b5eac1a15b85c0628
Bellardia <ian@elliian.com>
2016-09-20 16:38:28 -0400

    switch to pre-built nginx

--- swarm-nginx/attributes/default.rb
+++ swarm-nginx/attributes/default.rb
@@ -1,4 +1,4 @@
 default['nginx_swarm']['worker_processes']    = 4
 default['nginx_swarm']['worker_connections']  = 2048
 default['nginx_swarm']['certs']               = []
-default['nginx_swarm']['deps']               = ['build-essential', 'zlib1g-dev', 'libpcre3', 'libpcre3-dev', 'unzip', 'memcached']
+default['nginx_swarm']['deps']               = ['build-essential', 'zlib1g-dev', 'libpcre3', 'libpcre3-dev', 'unzip']
