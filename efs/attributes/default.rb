933ce59d2f11a2e7543d64882ba4e7787c6d091b
Hao-Jumbleberry <hao.ling@jumbleberry.com>
2016-09-21 11:14:19 -0400

    Update efs recipe to avoid EFS name tag conflict
    Do not rely on efs name tag but file system id from stack custom json

--- efs/attributes/default.rb
+++ efs/attributes/default.rb
@@ -1,14 +1,8 @@
 # AWS
-default['instance_identity_url'] = 'http://169.254.169.254/latest/dynamic/instance-identity/document'
 default['availability_zone_url'] = 'http://169.254.169.254/latest/meta-data/placement/availability-zone'
 
 # Config
 default['efs_mount_point'] = '/html'
-
-# Regex
-default['region_reg'] = '(?<=region\"\s:\s\")[^\"]*'
-default['efs_name_reg'] = '\"Name\":\s\"efs\"'
-default['file_system_id_reg'] = '(?<=FileSystemId\":\s\")[^\"]*'
 
 # Packages
 default['efs']['packages'] = [
--- efs/recipes/default.rb
+++ efs/recipes/default.rb
@@ -21,9 +21,7 @@ end
 # Mount EFS
 execute "mount-efs" do
     command <<-EOF
-        awsRegion=$(curl -s #{node['instance_identity_url']} | grep -ioP '#{node['region_reg']}')
-        fileSystemId=$(aws efs --region $awsRegion describe-file-systems | grep -iC 5 '#{node['efs_name_reg']}' | grep -ioP '#{node['file_system_id_reg']}')
-        mount -t nfs4 -o vers=4.1 $(curl -s #{node['availability_zone_url']}).$fileSystemId.efs.$awsRegion.amazonaws.com:/ #{node['efs_mount_point']}
+        mount -t nfs4 -o vers=4.1 $(curl -s #{node['availability_zone_url']}).#{node['fileSystemId']}.efs.#{node['awsRegion']}.amazonaws.com:/ #{node['efs_mount_point']}
         EOF
     user 'root'
 end
