6be5161753d7a61f02e493210ad97880301c93a3
Hao-Jumbleberry <hao.ling@jumbleberry.com>
2016-11-17 16:26:17 -0500

    Add efs sync to pull code from github

---
+++ efs/recipes/sync.rb
@@ -0,0 +1,35 @@
+include_attribute "sync"
+
+# Create swarm repo private key
+file node['swarm']['ssh_key'] do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  content node['swarmAppSshKey']
+  action :create_if_missing
+end
+
+# Sync swarm repo from Github
+git node['swarm']['path'] do
+  repository node['swarm']['url']
+  revision node['swarm']['revision']
+  action :sync
+  ssh_wrapper "ssh -i #{node['swarm']['ssh_key']}"
+end
+
+# Create integration repo private key
+file node['integration']['ssh_key'] do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  content node['integrationAppSshKey']
+  action :create_if_missing
+end
+
+# Sync integration repo from Github
+git node['integration']['path'] do
+  repository node['integration']['url']
+  revision node['integration']['revision']
+  action :sync
+  ssh_wrapper "ssh -i #{node['integration']['ssh_key']}"
+end
