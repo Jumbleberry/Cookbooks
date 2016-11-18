3ba7a681802ef82014d6255c3cd14c445ab453a3
Hao-Jumbleberry <hao.ling@jumbleberry.com>
2016-11-18 10:39:16 -0500

    Use execute resource instead of git resource

--- efs/recipes/sync.rb
+++ efs/recipes/sync.rb
@@ -2,36 +2,30 @@
 file node['swarm']['ssh_key'] do
   owner 'root'
   group 'root'
-  mode '0755'
+  mode '0400'
   content node['swarmAppSshKey']
   action :create_if_missing
 end
 
-ENV['GIT_SSH'] = '/usr/bin/ssh'
-
 # Sync swarm repo from Github
-git node['swarm']['path'] do
-  repository node['swarm']['url']
-  revision node['swarm']['revision']
-  ssh_wrapper "ssh -i #{node['swarm']['ssh_key']}"
-  user 'root'
-  action :sync
+execute "sync-swarm-repo" do
+    cwd node['swarm']['path']
+    command "ssh-agent bash -c 'ssh-add #{node['swarm']['ssh_key']}; git clone -b #{node['swarm']['revision']} git@#{node['swarm']['url']}'"
+    user 'root'
 end
 
 # Create integration repo private key
 file node['integration']['ssh_key'] do
   owner 'root'
   group 'root'
-  mode '0755'
+  mode '0400'
   content node['integrationAppSshKey']
   action :create_if_missing
 end
 
 # Sync integration repo from Github
-git node['integration']['path'] do
-  repository node['integration']['url']
-  revision node['integration']['revision']
-  ssh_wrapper "ssh -i #{node['integration']['ssh_key']}"
-  user 'root'
-  action :sync
+execute "sync-integration-repo" do
+    cwd node['integration']['path']
+    command "ssh-agent bash -c 'ssh-add #{node['integration']['ssh_key']}; git clone -b #{node['integration']['revision']} git@#{node['integration']['url']}'"
+    user 'root'
 end
