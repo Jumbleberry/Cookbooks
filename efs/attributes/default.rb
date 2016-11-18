07bd73c251a5957a1d34cd6f08a57c13c5ad3c6e
Hao-Jumbleberry <hao.ling@jumbleberry.com>
2016-11-18 10:57:17 -0500

    Implement my own git sync

--- efs/attributes/default.rb
+++ efs/attributes/default.rb
@@ -13,10 +13,12 @@ default['efs']['packages'] = [
 default['swarm']['path'] = node['efsMountPoint']
 default['swarm']['url'] = 'github.com:Jumbleberry/Swarm.git'
 default['swarm']['revision'] = 'master'
+default['swarm']['name'] = 'swarm'
 default['swarm']['ssh_key'] = ENV['HOME'] + '/.ssh/swarm_github_key'
 
 # Integration code
 default['integration']['path'] = node['efsMountPoint'] + '/integration'
 default['integration']['url'] = 'github.com:Jumbleberry/Campaign-Library.git'
 default['integration']['revision'] = 'swarm'
+default['integration']['name'] = 'integration'
 default['integration']['ssh_key'] = ENV['HOME'] + '/.ssh/integration_github_key'
--- efs/recipes/sync.rb
+++ efs/recipes/sync.rb
@@ -7,10 +7,19 @@ file node['swarm']['ssh_key'] do
   action :create_if_missing
 end
 
+# Clone swarm repo from Github if none exist
+execute "clone-swarm-repo" do
+    cwd node['swarm']['path']
+    command "ssh-agent bash -c 'ssh-add #{node['swarm']['ssh_key']}; git clone -b #{node['swarm']['revision']} git@#{node['swarm']['url']} #{node['swarm']['name']}'"
+    not_if { ::File.directory?("#{node['swarm']['path']}/#{node['swarm']['name']}") }
+    user 'root'
+end
+
 # Sync swarm repo from Github
 execute "sync-swarm-repo" do
-    cwd node['swarm']['path']
-    command "ssh-agent bash -c 'ssh-add #{node['swarm']['ssh_key']}; git clone -b #{node['swarm']['revision']} git@#{node['swarm']['url']}'"
+    cwd node['swarm']['path'] + '/' + node['swarm']['name']
+    command "ssh-agent bash -c 'ssh-add #{node['swarm']['ssh_key']}; git pull git@#{node['swarm']['url']} #{node['swarm']['revision']}'"
+    only_if { ::File.directory?("#{node['swarm']['path']}/#{node['swarm']['name']}") }
     user 'root'
 end
 
@@ -23,9 +32,18 @@ file node['integration']['ssh_key'] do
   action :create_if_missing
 end
 
+# Clone integration repo from Github if none exist
+execute "clone-integration-repo" do
+    cwd node['integration']['path']
+    command "ssh-agent bash -c 'ssh-add #{node['integration']['ssh_key']}; git clone -b #{node['integration']['revision']} git@#{node['integration']['url']} #{node['integration']['name']}'"
+    not_if { ::File.directory?("#{node['integration']['path']}/#{node['integration']['name']}") }
+    user 'root'
+end
+
 # Sync integration repo from Github
 execute "sync-integration-repo" do
-    cwd node['integration']['path']
-    command "ssh-agent bash -c 'ssh-add #{node['integration']['ssh_key']}; git clone -b #{node['integration']['revision']} git@#{node['integration']['url']}'"
+    cwd node['integration']['path'] + '/' + node['integration']['name']
+    command "ssh-agent bash -c 'ssh-add #{node['integration']['ssh_key']}; git pull git@#{node['integration']['url']} #{node['integration']['revision']}'"
+    only_if { ::File.directory?("#{node['integration']['path']}/#{node['integration']['name']}") }
     user 'root'
 end
