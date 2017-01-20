acecba9f76ac1e0e5a34ba1299c194fbde7e7001
Bellardia <ian@elliian.com>
2017-01-20 12:15:02 -0500

    Revert "Enable HP sync"
    This reverts commit b0fe0e2c8ede6a3a000a6a6640b581c6637af98b.

--- jb_deploy/recipes/admin_crons.rb
+++ jb_deploy/recipes/admin_crons.rb
@@ -3,7 +3,7 @@ cron "Admin - Sync Hitpath" do
   command "/usr/bin/php #{node[:admin][:path]}/cron_scripts/sync_hitpath.php"
   minute '*'
   user 'www-data'
-  action :create
+  action :delete
 end
 
 cron "Admin - Sale Projections" do
