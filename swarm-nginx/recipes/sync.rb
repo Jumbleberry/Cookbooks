a733983d97fdbb07c149511be32dca586293081e
Bellardia <ian@elliian.com>
2017-03-21 10:42:24 -0400

    clear shared memory on deploy

--- swarm-nginx/recipes/sync.rb
+++ swarm-nginx/recipes/sync.rb
@@ -1,6 +1,31 @@
 # Sleep for 3 minutes to wait for deployment
 execute 'sleep 180'
 
+# Empty out memcached
+execute 'clear memcached' do
+    command 'echo 'flush_all' | nc localhost 11211'
+    user 'root'
+    action :run
+end
+
+# empty out shared memory
+["ssi", "php", "ps", "ps/file", "ps/memory"].each do |type|
+    directory '/dev/shm/' + type do
+        owner 'www-data'
+        group 'www-data'
+        mode 0664
+        action :create
+        recursive true
+    end
+    directory '/dev/shm/' + type do
+        owner 'www-data'
+        group 'www-data'
+        mode 0664
+        action :create
+        recursive true
+    end
+end
+
 service "nginx" do 
     provider Chef::Provider::Service::Upstart
     ignore_failure true
