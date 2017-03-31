58f92449d62086cfe95357ce4d654ae268df7985
Bellardia <ian@elliian.com>
2017-03-31 14:36:44 -0400

    Add additional crons

--- jb_deploy/recipes/mesh_crons.rb
+++ jb_deploy/recipes/mesh_crons.rb
@@ -2,6 +2,20 @@ cron "Mesh - Cache Buster" do
   command "/usr/bin/php #{node[:jbx][:mesh][:path]}/crons/cache_buster.php"
   minute '*'
   user 'www-data'
+  action :delete
+end
+
+cron "Mesh - Bridge Cache Buster" do
+  command "/usr/bin/php #{node[:jbx][:mesh][:path]}/crons/bridge_cache_buster.php"
+  minute '*'
+  user 'www-data'
+  action :create
+end
+
+cron "Mesh - Campaign Cache Buster" do
+  command "/usr/bin/php #{node[:jbx][:mesh][:path]}/crons/campaign_cache_buster.php"
+  minute '*'
+  user 'www-data'
   action :create
 end
 
