e15578293560eb16c7d23126778345659defc063
Bellardia <ian@elliian.com>
2017-04-20 15:15:23 -0400

    min_spare_servers != max_spare_servers bomb

--- swarm-php/attributes/default.rb
+++ swarm-php/attributes/default.rb
@@ -4,9 +4,9 @@ default['php']['fpm']['display_errors'] = 'Off'
 default['php']['fpm']['listen'] = '0.0.0.0:9000'
 default['php']['fpm']['pm'] = 'dynamic'
 default['php']['fpm']['max_children'] = '256'
-default['php']['fpm']['start_servers'] = '128'
-default['php']['fpm']['min_spare_servers'] = '64'
-default['php']['fpm']['max_spare_servers'] = '64'
+default['php']['fpm']['start_servers'] = '64'
+default['php']['fpm']['min_spare_servers'] = '32'
+default['php']['fpm']['max_spare_servers'] = '128'
 default['php']['fpm']['include_path'] = '.:/usr/share/php:/var/www/lib'
 
 default['php']['fpm']['conf_dirs'] = ['/etc/php/7.0/mods-available']
