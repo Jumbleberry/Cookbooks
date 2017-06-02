abbe209d4ae67bd9068b5f65b542f1d312b429e9
Bellardia <ian@elliian.com>
2017-06-02 12:20:25 -0400

    remove accidental code inclusion

--- gearman/attributes/default.rb
+++ gearman/attributes/default.rb
@@ -3,4 +3,3 @@ default["gearman"]["source"]["version"] = "1.1.12"
 default["gearman"]["ip"]                = (node[:network][:interfaces][node[:network_interface]] || node[:network][:interfaces][:enp0s8])[:addresses].detect{|k,v| v[:family] == "inet" }.first
 default['services']['gearman']['host']  = node[:services][:gearman][:host] || "gearman.service.consul"
 default['services']['gearman']['port']  = node[:services][:gearman][:port] || "4730"
-node[:network][:interfaces][:enp0s8][:addresses].detect{|k,v| v[:family] == "inet" }.first
