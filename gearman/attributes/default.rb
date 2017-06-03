4c0fc0efdb47753a46dbe466d28aca7716d0edac
Bellardia <ian@elliian.com>
2017-06-02 23:48:39 -0400

    support more network options

--- gearman/attributes/default.rb
+++ gearman/attributes/default.rb
@@ -1,5 +1,10 @@
 default["gearman"]["version"]           = "1.0.*"
 default["gearman"]["source"]["version"] = "1.1.12"
-default["gearman"]["ip"]                = (node[:network][:interfaces][node[:network_interface]] || node[:network][:interfaces][:enp0s8])[:addresses].detect{|k,v| v[:family] == "inet" }.first
+default["gearman"]["source"]["version"] = "1.1.16"
+default["gearman"]["ip"]                = (
+        node[:network][:interfaces][node[:network_interface]] || 
+        node[:network][:interfaces][:enp0s8] ||
+        node[:network][:interfaces][:ens3]
+    )[:addresses].detect{|k,v| v[:family] == "inet" }.first
 default['services']['gearman']['host']  = node[:services][:gearman][:host] || "gearman.service.consul"
 default['services']['gearman']['port']  = node[:services][:gearman][:port] || "4730"
