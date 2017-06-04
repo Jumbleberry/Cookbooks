f9c5799e60aac3f5553ed036790518ea237b76f2
Bellardia <ian@elliian.com>
2017-06-04 00:06:25 -0400

    fix os release

--- opsworks_stack_state_sync/recipes/motd.rb
+++ opsworks_stack_state_sync/recipes/motd.rb
@@ -3,13 +3,6 @@ node.default[:opsworks][:stack] = search("aws_opsworks_stack").first
 node.default[:opsworks][:layers] = search("aws_opsworks_layer").first
 node.default[:opsworks][:instance] = search("aws_opsworks_instance").first
 
-os_release =
-  if rhel7?
-    os_release = File.read("/etc/redhat-release").chomp
-  else
-    `head -1 /etc/issue | sed -e 's/ \\\\.*//'`.chomp
-  end
-
 template "/etc/motd.opsworks-static" do
   source "motd.erb"
   mode "0644"
@@ -17,6 +10,6 @@ template "/etc/motd.opsworks-static" do
     :stack => node[:opsworks][:stack],
     :layers => node[:opsworks][:layers],
     :instance => node[:opsworks][:instance],
-    :os_release => os_release
+    :os_release => node[:lsb][:release]
   })
 end
