a9744c780e1e863c37399269e28b76b04e71775d
Bellardia <ian@elliian.com>
2017-06-03 23:09:11 -0400

    determine consul interface as well

--- web-server/attributes/default.rb
+++ web-server/attributes/default.rb
@@ -4,5 +4,8 @@ interfaces += ['eth1', 'eth0', 'enp0s8', 'ens3']
 interfaces.each do |interface|
     if node[:network][:interfaces].key?(interface)
         override![:network_interface] = interface
+        override![:consul][:bind_interface] = interface
+        override![:consul][:advertise_interface] = interface
+        override![:consul][:client_interface] = interface
     end
 end
