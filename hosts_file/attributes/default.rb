a73bfc7f363dceef4d67fe9299b5ee74128600da
Bellardia <ian@elliian.com>
2017-06-03 22:41:42 -0400

    Revert "and berks contents"
    This reverts commit 5241615d1fd1f380273f636df048b5a46200b5c5.

--- hosts_file/attributes/default.rb
+++
@@ -1,3 +0,0 @@
-default[:hosts_file][:path] = '/etc/hosts'
-default[:hosts_file][:define_self] = 'ip_address' # or 'loopback' or 'localhost_only'
-default[:hosts_file][:custom_entries] = {}
--- rc_mon/attributes/default.rb
+++
@@ -1,1 +0,0 @@
-default[:rc_mon] = {}
