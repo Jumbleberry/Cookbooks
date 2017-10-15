e3fef58c97b44588cbb5a4f23bcd8baab7a22659
Bellardia <ian@elliian.com>
2017-10-15 16:19:24 -0400

    Proactive Advertiser Shave Detection

--- jb_deploy/recipes/processing_crons.rb
+++ jb_deploy/recipes/processing_crons.rb
@@ -14,6 +14,14 @@ cron "Processing - Ensure Valid SSL Certificates Network Wide" do
   action :create
 end
 
+cron "Processing - Detect Advertiser Shave" do
+  command "/usr/bin/php #{node[:jbx][:processing][:path]}/crons/detect_shave.php"
+  hour '0,8,12,16,20'
+  minute '15'
+  user 'www-data'
+  action :create
+end
+
 cron "Processing - Detect CRM Features" do
   command "/usr/bin/php #{node[:jbx][:processing][:path]}/crons/detect_features.php"
   hour '2-23/6'
