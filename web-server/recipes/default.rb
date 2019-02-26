6d3c569261eafecfa47c02715b3c4fb2bf8d68ef
Bellardia <ian@jumbleberry.com>
2019-02-26 10:41:30 -0500

    formatting/backporting fixes from v2

--- web-server/recipes/default.rb
+++ web-server/recipes/default.rb
@@ -1,16 +1,13 @@
 include_recipe "timezone-ii"
 
 # IF not amazon - update hosts file
-if ( !node.attribute?('ec2') || !node[:ec2].attribute?('instance_id') || !/(i|snap|vol)-[a-zA-Z0-9]+/.match(node[:ec2][:instance_id]))
-
-    template '/etc/hosts' do
-      source "vagrant_hosts.erb"
-      mode "0644"
-    end
+if (!node.attribute?("ec2") || !node[:ec2].attribute?("instance_id") || !/(i|snap|vol)-[a-zA-Z0-9]+/.match(node[:ec2][:instance_id]))
+  template "/etc/hosts" do
+    source "vagrant_hosts.erb"
+    mode "0644"
+  end
 else
-    
-    include_recipe "opsworks_stack_state_sync"
-
+  include_recipe "opsworks_stack_state_sync"
 end
 
 #System configurations
@@ -20,13 +17,13 @@ execute "sysctl-config" do
   action :nothing
 end
 
-template '/etc/sysctl.conf' do
-  source 'sysctl.conf.erb'
-  user 'root'
+template "/etc/sysctl.conf" do
+  source "sysctl.conf.erb"
+  user "root"
   variables ({
-    'shmmax' => node['system']['shared_memory']['shmmax'],
-    'shmall' => node['system']['shared_memory']['shmall'],
-    'shmmni' => node['system']['shared_memory']['shmmni']
+    "shmmax" => node["system"]["shared_memory"]["shmmax"],
+    "shmall" => node["system"]["shared_memory"]["shmall"],
+    "shmmni" => node["system"]["shared_memory"]["shmmni"],
   })
   notifies :run, "execute[sysctl-config]", :immediately
 end
@@ -34,10 +31,11 @@ end
 # Disable transparent huge pages
 execute "disable-thp" do
   command "echo madvise | sudo tee /sys/kernel/mm/transparent_hugepage/enabled"
+  not_if "cat /sys/kernel/mm/transparent_hugepage/enabled | grep  '\[madvise\]'"
 end
 
 #System packages
-syspackages = ['git', 'gcc', 'vim', 'libpcre3-dev', 'make', 'curl', 'unzip', 'uuid']
+syspackages = ["git", "gcc", "vim", "libpcre3-dev", "make", "curl", "unzip", "uuid"]
 syspackages.each do |pkg|
   package "#{pkg}" do
     action :install
@@ -45,51 +43,48 @@ syspackages.each do |pkg|
 end
 
 # we're running on a vagrant machine - make it easier to manage /var/www/
-if File.directory?('/home/vagrant')
-    
-    directory '/var/www' do
-      recursive true
-      action :delete
-      not_if { File.symlink?('/var/www') }
-    end
+if File.directory?("/home/vagrant")
+  directory "/var/www" do
+    recursive true
+    action :delete
+    not_if { File.symlink?("/var/www") }
+  end
 
-    # Makes sure that the www directory exists
-    directory '/vagrant/www' do
-        owner node['user']
-        group node['user']
-        action :create
-    end
-    
-    link '/var/www' do
-      to '/vagrant/www/'
-      owner node['user']
-      group node['user']
-      action :create
-      not_if { File.symlink?('/var/www') }
-    end
+  # Makes sure that the www directory exists
+  directory "/vagrant/www" do
+    owner node["user"]
+    group node["user"]
+    action :create
+  end
 
-# Not on vagrant - use a normal directory
-else 
-    
-    link '/var/www' do
-      to '/vagrant/www/'
-      action :delete
-      only_if { File.symlink?('/var/www') }
-    end
-    
-    # Make sure directory exists
-    directory "/var/www/" do
-      owner node["user"]
-      group node["user"]
-      recursive true
-      action :create
-    end
-    
+  link "/var/www" do
+    to "/vagrant/www/"
+    owner node["user"]
+    group node["user"]
+    action :create
+    not_if { File.symlink?("/var/www") }
+  end
+
+  # Not on vagrant - use a normal directory
+else
+  link "/var/www" do
+    to "/vagrant/www/"
+    action :delete
+    only_if { File.symlink?("/var/www") }
+  end
+
+  # Make sure directory exists
+  directory "/var/www/" do
+    owner node["user"]
+    group node["user"]
+    recursive true
+    action :create
+  end
 end
 
-ssh_known_hosts_entry 'github.com'
+ssh_known_hosts_entry "github.com"
 
 # Install root certs on non-production
-if node['environment'] != 'production'
-    include_recipe 'web-server::ca'
+if node["environment"] != "production"
+  include_recipe "web-server::ca"
 end
