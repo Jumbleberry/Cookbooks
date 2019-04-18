1c5f43a85e11535c466dfc71a221e57338dd7825
Bellardia <ian@jumbleberry.com>
2019-04-17 23:22:39 -0400

    Upgrade hosts.rb to support modern opsworks

--- opsworks_stack_state_sync/recipes/hosts.rb
+++ opsworks_stack_state_sync/recipes/hosts.rb
@@ -1,17 +1,15 @@
 require 'resolv'
 
-begin
-    node.default[:opsworks] = node[:opsworks] || {}
-    node.default[:opsworks][:stack] = search("aws_opsworks_stack").first
-    node.default[:opsworks][:layers] = search("aws_opsworks_layer")
-    node.default[:opsworks][:instance] = search("aws_opsworks_instance").first
-rescue
-end
-
 template '/etc/hosts' do
   source "hosts.erb"
   mode "0644"
   variables(
-    :localhost_name => node[:opsworks][:instance][:hostname]
+    :localhost_name => node.attribute?(:opsworks)? node[:opsworks][:instance][:hostname]:  node[:hostname],
+    :nodes => search(:node, "name:*")
   )
+end
+
+ohai 'hostname' do
+  plugin 'hostname'
+  action :reload
 end
