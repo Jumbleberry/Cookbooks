47e722bf73ca6f4e2ec6f9e5d5ba6fe46322c10b
Bellardia <ian@jumbleberry.com>
2019-04-17 23:28:18 -0400

    Upgrade Motd (#30)
    * Update motd.rb
    
    * Update motd.erb

--- opsworks_stack_state_sync/recipes/motd.rb
+++ opsworks_stack_state_sync/recipes/motd.rb
@@ -1,18 +1,19 @@
-begin
-    node.default[:opsworks] = node[:opsworks] || {}
-    node.default[:opsworks][:stack] = search("aws_opsworks_stack").first
-    node.default[:opsworks][:layers] = search("aws_opsworks_layer")
-    node.default[:opsworks][:instance] = search("aws_opsworks_instance").first
-rescue
-end
+if node.attribute?(:opsworks)
+  os_release =
+    if rhel7?
+      os_release = File.read("/etc/redhat-release").chomp
+    else
+      `head -1 /etc/issue | sed -e 's/ \\\\.*//'`.chomp
+    end
 
-template "/etc/motd.opsworks-static" do
-  source "motd.erb"
-  mode "0644"
-  variables({
-    :stack => node[:opsworks][:stack],
-    :layers => node[:opsworks][:layers],
-    :instance => node[:opsworks][:instance],
-    :os_release => node[:lsb][:release]
-  })
+  template "/etc/motd.opsworks-static" do
+    source "motd.erb"
+    mode "0644"
+    variables({
+      :stack => node[:opsworks][:stack],
+      :layers => node[:opsworks][:layers],
+      :instance => node[:opsworks][:instance],
+      :os_release => os_release
+    })
+  end
 end
