17c7598335a00ef7e4e9568f7f920c2d47f557f4
Bellardia <ian@jumbleberry.com>
2019-07-02 22:18:25 -0400

    Add images bucket

--- admin/attributes/default.rb
+++ admin/attributes/default.rb
@@ -1,3 +1,4 @@
 default['admin']['hostname']                = 'admin.jumbleberry.com'
 default['admin']['path']                    = '/var/www/admin/'
 default['admin']['environment']             = ''
+default['admin']['images']                  = 'jbimages-production'
