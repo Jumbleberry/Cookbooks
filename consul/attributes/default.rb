a9c431aa6c71ef567bb2d2fa3a1e44dcbcf56231
Bellardia <ian@jumbleberry.com>
2019-10-18 10:00:23 -0400

    Remove rpc addr

--- consul/attributes/default.rb
+++ consul/attributes/default.rb
@@ -126,7 +126,6 @@ default['consul']['system_account'] = false
 default['consul']['ports'] = {
   'dns'      => 8600,
   'http'     => 8500,
-  'rpc'      => 8400,
   'serf_lan' => 8301,
   'serf_wan' => 8302,
   "server"   => 8300,
